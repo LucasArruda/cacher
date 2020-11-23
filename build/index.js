75887a429f20c29092d80d61cc6a3021d5adeae3
lucasarruda <lucasarruda@gmail.com>
2020-11-22 22:36:54 -0300

    Fix express loading

--- build/index.js
+++ build/index.js
@@ -1,13 +1,73 @@
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 (function (factory) {
     if (typeof module === "object" && typeof module.exports === "object") {
         var v = factory(require, exports);
         if (v !== undefined) module.exports = v;
     }
     else if (typeof define === "function" && define.amd) {
-        define(["require", "exports"], factory);
+        define(["require", "exports", "./cacher", "express"], factory);
     }
 })(function (require, exports) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
+    var cacher_1 = __importDefault(require("./cacher"));
+    var express_1 = __importDefault(require("express"));
+    var API_URL = '/api/v1/';
+    var app = express_1.default();
+    cacher_1.default.set('test', 'oi');
+    console.log('testing', cacher_1.default.get('test'));
+    var ok = function (obj) { return (typeof obj !== 'undefined') && obj !== null; };
+    app.get(API_URL + "get/:key", function (req, res) {
+        var key = req.params.key;
+        var result = cacher_1.default.get(key);
+        if (ok(result)) {
+            res.status(200).send({
+                success: 'true',
+                message: 'Object retrieved',
+                result: result
+            });
+        }
+        else {
+            res.status(404).send({
+                success: 'false',
+                message: 'Object not found',
+                error: "Object was not found with key: " + key
+            });
+        }
+    });
+    app.get(API_URL + "has/:key", function (req, res) {
+        var key = req.params.key;
+        var result = cacher_1.default.has(key);
+        res.status(200).send({
+            success: 'true',
+            message: result ? 'Object found' : 'Object not found',
+            result: result ? 'Object found' : 'Object not found'
+        });
+    });
+    app.post(API_URL + "set", function (req, res) {
+        var key = req.params.key;
+        var value = req.params.value;
+        var result = cacher_1.default.get(key);
+        if (ok(result)) {
+            res.status(200).send({
+                success: 'true',
+                message: 'Object retrieved',
+                result: result
+            });
+        }
+        else {
+            res.status(404).send({
+                success: 'false',
+                message: 'Object not found',
+                error: "Object was not found with key: " + key
+            });
+        }
+    });
+    var PORT = 5000;
+    app.listen(PORT, function () {
+        console.log("server running on port " + PORT);
+    });
 });
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,22 @@
 import Cacher from './cacher';
-import { Console } from 'console';
+import express from 'express';
+import { Router, Request, Response, NextFunction } from 'express';
 
 const API_URL = '/api/v1/';
-const express = require( "express" );
 const app = express();
+
 
 const ok = (obj: Object | undefined): boolean => (typeof obj !== 'undefined') && obj !== null;
 
-app.get(`${API_URL}get/:key`, (req, res) => {
+app.get(`${API_URL}get/:key`, (req : Request, res : Response) => {
   const key : string = req.params.key;
-  const result: Object | undefined = Cacher.get(key);
-
-app.get(`${API_URL}has/:key`, (req, res) => {
-  const key : string = req.params.key;
-  const result : boolean = Cacher.has(key);
-
-  res.status(200).send({
-    success: 'true',
-    message: result ? 'Object found' : 'Object not found',
-    data: result ? 'Object found' : 'Object not found'
-  });
-});
+  const result : Object | undefined = Cacher.get(key);
 
   if (ok(result)) {
     res.status(200).send({
       success: 'true',
       message: 'Object retrieved',
-      object: result
+      result: result
     });
   } else {
     res.status(404).send({
