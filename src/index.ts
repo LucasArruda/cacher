c36874e8e4524d811919742fdc542faff68c45b5
lucasarruda <lucasarruda@gmail.com>
2020-11-22 21:33:14 -0300

    /get/:key

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,31 @@
 import Cacher from './cacher';
+import { Console } from 'console';
+import express from 'express';
+
+const API_URL = '/api/v1/';
+const app = express();
+
+const ok = (obj: Object | undefined): boolean => (typeof obj !== 'undefined') && obj !== null;
+
+app.get(`${API_URL}get/:key`, (req, res) => {
+  const key : string = req.params.key;
+  const result: Object | undefined = Cacher.get(key);
+
+  if (ok(result)) {
+    res.status(200).send({
+      success: 'true',
+      message: 'Object retrieved',
+      object: result
+    });
+  } else {
+    res.status(404).send({
+      success: 'false',
+      message: 'Object not found',
+      error: `Object was not found with key: ${key}`
+    });
+  }
+});
+const PORT = 5000;
+app.listen(PORT, () => {
+  console.log(`server running on port ${PORT}`)
+});
